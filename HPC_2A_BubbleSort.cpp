adba4995d364aecf642222cb9dc60a80e66fdc0e
Dipak Jagtap <dipakjagtap321@gmail.com>
2023-05-22 15:02:28 +0530

    Update HPC_2A_BubbleSort.cpp

--- HPC_2A_BubbleSort.cpp
+++ HPC_2A_BubbleSort.cpp
@@ -41,10 +41,10 @@ void swap(int &a, int &b)
 int main()
 {
  	int *a,n;
- 	cout<<"\n enter total no of elements=>";
+ 	cout<<"\n enter total no of elements:";
  	cin>>n;
  	a=new int[n];
- 	cout<<"\n enter elements=>";
+ 	cout<<"\n enter elements:";
  	for(int i=0;i<n;i++)
  	{
  		cin>>a[i];
@@ -52,7 +52,7 @@ int main()
  
  	bubble(a,n);
  
- 	cout<<"\n sorted array is=>";
+ 	cout<<"\n sorted array is:"<<endl;
  	for(int i=0;i<n;i++)
  	{
  		cout<<a[i]<<endl;
@@ -60,4 +60,3 @@ int main()
 
 return 0;
 }
-
